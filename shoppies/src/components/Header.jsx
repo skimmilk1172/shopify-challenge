024575fe4d35773132e92b1d90788af45131e7a0
Sean Kim <sean.sihun.kim@gmail.com>
2021-01-15 01:46:34 +0000

    add example header, playing around with color

---
+++ shoppies/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.scss';
+
+function Header () {
+  return (
+    <div>
+      <header>
+        <h1 className='title'>The Shoppies</h1>
+      </header>
+    </div>
+  )
+};
+
+export default Header;
